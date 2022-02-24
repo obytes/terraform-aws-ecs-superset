e2efed19eb22438394f9da75a176cdfe43b93357
Evan Danaher <edanaher@admithub.com>
2022-02-24 16:50:22 -0500

    Core app: EFS.

--- stacks/aws/superset-core-apps/efs.tf
+++ stacks/aws/superset-core-apps/efs.tf
@@ -2,9 +2,10 @@ module "app_efs" {
   source      = "../../../modules/aws/efs/"
   common_tags = var.common_tags
   service_sg_id = {
-    superset_app  = module.app.app_service_security_group_id
-    superset_wrk  = module.worker.ecs_service_security_group_id
-    superset_beat = module.worker_beat.beat_ecs_service_security_group_id
+    # TODO: Re-enable once these modules are added.
+    # superset_app  = module.app.app_service_security_group_id
+    # superset_wrk  = module.worker.ecs_service_security_group_id
+    # superset_beat = module.worker_beat.beat_ecs_service_security_group_id
   }
   private_subnet_ids = var.private_subnet_ids
   prefix             = var.prefix
