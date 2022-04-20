f238e298a53f82c03866c4101463441e192e1cfb
Evan Danaher <edanaher@admithub.com>
2022-04-20 12:24:25 -0400

    Raise log retention time period for SOC2 purposes.

--- modules/aws/ecs/cloudwatch_logs.tf
+++ modules/aws/ecs/cloudwatch_logs.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "main" {
   name              = "/aws/ecs/${local.prefix}"
-  retention_in_days = local.common_tags["env"] == "prd" ? 0 : 30
+  retention_in_days = local.common_tags["env"] == "prd" ? 0 : 365
   kms_key_id        = var.kms_arn
 
   tags = local.common_tags
